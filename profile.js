01b086c9f6cc56a5bba69110d13d025ae61f7e21
Sean <seanroscady@gmail.com>
2025-10-30 16:36:19 +0800

    made changes to profile.js, customized avatar,name,role,bio,tags, and facts

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
   avatar: "assets/img/cd093021-8ead-4883-8207-23c1429d16e6.jpg",
-  name: "Sean Chester,
-  role: "Aspiring ???",
-  bio: "Mahilig ako kumain at matulog",
+  name: "Sean Chester",
+  role: "Aspiring Developer",
+  bio: "Computer science student eager to learn new skills!",
   tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "C", "C++", "Java"],
   facts: [
-    { label: "Nickname",      value: "Sean" },
+    { label: "Nickname",      value: "Shoti" },
     { label: "Favorite Food", value: "Ramen 🍜" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Gaming 👾" },
+    { label: "Hobby",         value: "Gaming 👾, Listening to Music 🎧, Watching Movies 🎥" },
     { label: "Location",      value: "Quezon City, PH 📍" }
   ]
 };
